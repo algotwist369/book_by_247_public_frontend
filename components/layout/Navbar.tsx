2761450d6e5eed2bf44ff647607fec66a40001b9
Ankit Pathak <info.algotwist@gmail.com>
2026-05-31 15:24:14 +0530

    minor navbar fix

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -75,7 +75,7 @@ export const Navbar = () => {
     return (
         <nav className="sticky top-0 z-50 w-full bg-white shadow-sm">
             {/* Top Tier */}
-            <div className="relative flex items-center justify-between py-3 md:py-4 border-b border-zinc-50 max-w-7xl mx-auto">
+            <div className="relative flex items-center justify-between py-3 md:py-4 border-b border-zinc-50 max-w-7xl mx-auto px-4 md:px-0">
                 <div className="flex items-center gap-3 sm:gap-4 md:gap-8">
                     <Link href="/" className="shrink-0 flex items-center">
                         <img
